4c57c9d5328410d6b18ad7c2d3ce57cc4ea27d1f
Robert Martinis <robertmartinis@hotmail.com>
2023-11-09 18:51:19 +0100

    Refactor

--- app/src/screens/Recipes.js
+++ app/src/screens/Recipes.js
@@ -1,73 +1,72 @@
 import axios from 'axios';
-import './Recipes.css'
+import './Recipes.css';
 import { useState, useEffect } from 'react';
-import Recipe from '../components/Recipe.js'
-
-
+import Recipe from '../components/Recipe.js';
 
 function Recipes() {
   const [page, setPage] = useState(0);
-  const [recipes, setRecipes] = useState(null)
-  const [currentRecipe, selectRecipe] = useState(null)
+  const [recipes, setRecipes] = useState([]);
+  const [currentRecipe, setCurrentRecipe] = useState(null);
 
   useEffect(() => {
-    getRecipes(1);
-  }, []);
+    fetchRecipes();
+  }, [page]); 
 
-  async function getRecipes(amount) {
-    let ingredients = ['tomato', 'egg'] // TODO: This should be based on ingredients specified by the user
-    let ingredientsQuery = ''
-    for (var ingredient of ingredients) {
-      ingredientsQuery += `ingredients=${ingredient}&`
-    }
-    ingredientsQuery = ingredientsQuery.slice(0, -1)
-    const hasItems = await axios.get(`http://localhost:8080/recipes?page=${page + amount}&${ingredientsQuery}`).then((response) => {
-      if (response.data.length == 0)
-        return false
-      const recipeCards = response.data.map((recipe) => (
-        <div key={recipe.Title} className="card">
-          <div className="title">{recipe['Title']}</div>
-          <div className="cardContainer">
-            <img onClick={() => selectRecipe(recipe)} className="image" src={recipe['ImageSrc']} alt='' />
-            <div className="completenessContainer">
-              <div className="completeness">Ingredients</div>
+  const fetchRecipes = async () => {
+    const ingredients = ['tomato', 'egg']; // This should be dynamic based on user input
+    const ingredientsQuery = ingredients.map(ing => `ingredients=${ing}`).join('&');
+    
+    try {
+      const response = await axios.get(`http://localhost:8080/recipes?page=${page}&${ingredientsQuery}`);
+      
+      if (response.data.length === 0) {
+        // Handle no recipes found, possibly reset to first page or show a message
+      } else {
+        const recipeElements = response.data.map(recipe => (
+          <div key={recipe.Title} className="card">
+            <div className="title">{recipe['Title']}</div>
+            <div className="cardContainer">
+              <img onClick={() => setCurrentRecipe(recipe)} className="image" src={recipe['ImageSrc']} alt='' />
+              <div className="completenessContainer">
+                <div className="completeness">Ingredients</div>
+              </div>
             </div>
           </div>
-        </div>))
-      
-      setRecipes(recipeCards);
-      return true
-    })
-    if (hasItems && (page != 1 || amount != -1)) {
-      setPage(page + amount);
+        ));
+        setRecipes(recipeElements);
+      }
+    } catch (error) {
+      // Handle error, possibly show a message to the user
     }
-    
-  }
+  };
+
+  const handlePageChange = (newPage) => {
+    if (newPage >= 0) setPage(newPage);
+  };
 
   return (
     <div className="container">
-
-      {currentRecipe ? <Recipe recipe={currentRecipe}/> : 
-      
-      <><div className="cardsContainer">
-        {recipes ?
-          recipes :
-          null}
-      </div>
-      <div className="buttonNavigationContainer">
-        <button className="button" onClick={() => {
-          getRecipes(-1)
-        }}>{`Previous Page`}
-        </button>
-        <div>{`Page ${page}`}</div>
-        <button className="button" onClick={() => {
-          getRecipes(1)
-        }}>{`Next Page`}
-        </button>
-      </div></>}
-      
+      {currentRecipe ? (
+        <Recipe recipe={currentRecipe}/>
+      ) : (
+        <>
+          <div className="cardsContainer">
+            {recipes.length > 0 ? recipes : <p>No recipes found.</p>}
+          </div>
+          <div className="buttonNavigationContainer">
+            <button className="button" onClick={() => handlePageChange(page - 1)}>
+              Previous Page
+            </button>
+            <div>{`Page ${page + 1}`}</div>
+            <button className="button" onClick={() => handlePageChange(page + 1)}>
+              Next Page
+            </button>
+          </div>
+        </>
+      )}
     </div>
   );
 }
 
 export default Recipes;
+

0f6c39a13c0b27f119d39222229cba873d2514bc
Robert Martinis <robertmartinis@hotmail.com>
2023-11-11 13:06:19 +0100

    fix: Search if title contains rather than begins with

--- server/server.js
+++ server/server.js
@@ -31,21 +31,21 @@ app.get('/recipes', async (req, res) => {
     })
     const result = await db.all(`SELECT * FROM Recipes WHERE (${ingredientsQuery}) LIMIT 8 OFFSET ${(page - 1)* 8};`)
     res.send(result)
-    db.close()
+    await db.close()
 })
 
 app.get('/recipes/search', async (req, res) => { 
     const page = req.query.page
     console.log(page)
-    let nameStartsWith = req.query.title || ''; // Default to an empty string if no name provided
+    let titleContains = req.query.title || ''; // Default to an empty string if no name provided
     let limit = parseInt(req.query.limit, 8) || 8; // Default to 10 if no limit provided
     const db = await sqlite.open({
         filename: '../db/recipes.db',
         driver: sqlite3.Database
     })
   
-  const result = await db.all(`SELECT * FROM recipes WHERE Title LIKE ? LIMIT ? OFFSET ? `, [nameStartsWith + '%', limit, (page-1)*8])
+  const result = await db.all(`SELECT * FROM recipes WHERE Title LIKE ? LIMIT ? OFFSET ? `, ['%' + titleContains + '%', limit, (page-1)*8])
   res.send(result)
-  db.close()
+  await db.close()
 })
 

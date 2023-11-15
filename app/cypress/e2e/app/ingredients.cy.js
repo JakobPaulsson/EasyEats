bb35becb92a0167f5737189c8dfed37e2b6d32d6
Robert Martinis <robertmartinis@hotmail.com>
2023-11-15 06:59:50 +0100

    Reformat ingredient page tests

--- app/cypress/e2e/app/ingredients.cy.js
+++ app/cypress/e2e/app/ingredients.cy.js
@@ -16,9 +16,7 @@ describe("Using the ingredients page", () => {
     cy.get("[data-cy=addIngredient]")
       .find("input")
       .should("have.value", "milk");
-  })
-
-
+  });
 
   it("Should not be able to add ingredient to inventory with invalid amount and unit", () => {
     cy.get("[data-cy=addIngredient]").should("have.value", "");
@@ -26,12 +24,12 @@ describe("Using the ingredients page", () => {
     cy.get("[data-cy=addIngredient]").type("milk{downArrow}{enter}");
     cy.get("[data-cy=addButton]").click();
 
-    cy.get("[data-cy=inventory]").should("have.length", 0).and('not.contain', 'NaN');
+    cy.get("[data-cy=inventory]")
+      .should("have.length", 0)
+      .and("not.contain", "NaN");
+  });
 
-    })
-
-  
-  it("Should not be able to add ingredient to inventory with invalid unit", () => {  
+  it("Should not be able to add ingredient to inventory with invalid unit", () => {
     cy.get("[data-cy=addIngredient]").should("have.value", "");
     cy.get("[data-cy=addIngredient]").click();
     cy.get("[data-cy=addIngredient]").type("milk{downArrow}{enter}");
@@ -43,12 +41,12 @@ describe("Using the ingredients page", () => {
     cy.get("[data-cy=addAmount]").find("input").should("have.value", 123);
     cy.get("[data-cy=addButton]").click();
 
-    cy.get("[data-cy=inventory]").should("have.length", 0).and('not.contain', 'NaN');
+    cy.get("[data-cy=inventory]")
+      .should("have.length", 0)
+      .and("not.contain", "NaN");
+  });
 
-    })
-
-
-  it("Should be able to add ingredient to inventory with valid inputs", () => {  
+  it("Should be able to add ingredient to inventory with valid inputs", () => {
     cy.get("[data-cy=addIngredient]").should("have.value", "");
     cy.get("[data-cy=addIngredient]").click();
     cy.get("[data-cy=addIngredient]").type("milk{downArrow}{enter}");
@@ -60,20 +58,15 @@ describe("Using the ingredients page", () => {
 
     cy.get("[data-cy=addUnit]").click();
     cy.get("ul>li").contains("gram").click();
-    cy.get("[data-cy=addUnit]").find("input").should("have.value", 'gram');
+    cy.get("[data-cy=addUnit]").find("input").should("have.value", "gram");
     cy.get("[data-cy=addButton]").click();
 
-    cy.get("[data-cy=inventory]").should("have.length", 1).and('not.contain', 'NaN');
+    cy.get("[data-cy=inventory]")
+      .should("have.length", 1)
+      .and("not.contain", "NaN");
 
-    cy.get("[data-cy=addIngredient]")
-      .find("input")
-      .should("have.value", "");
-    cy.get("[data-cy=addAmount]")
-      .find("input")
-      .should("have.value", "");
-    cy.get("[data-cy=addUnit]")
-      .find("input")
-      .should("have.value", "");
-
-    })
+    cy.get("[data-cy=addIngredient]").find("input").should("have.value", "");
+    cy.get("[data-cy=addAmount]").find("input").should("have.value", "");
+    cy.get("[data-cy=addUnit]").find("input").should("have.value", "");
   });
+});

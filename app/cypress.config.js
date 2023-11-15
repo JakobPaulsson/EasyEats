1b1abce58709aebc15be3c17b0162c0bf41647c9
Robert Martinis <robertmartinis@hotmail.com>
2023-11-15 10:22:33 +0100

    add cypress coverage

--- app/cypress.config.js
+++ app/cypress.config.js
@@ -7,11 +7,20 @@ module.exports = defineConfig({
       framework: "create-react-app",
       bundler: "webpack",
     },
+
+    env: {
+      codeCoverage: {
+        url: 'http://localhost:3003/__coverage__',
+      }
+    }
   },
 
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+    require("@cypress/code-coverage/task")(on, config);    
+    return config;
+
     },
+    baseUrl: 'http://localhost:3000',
   },
 });

3dcbacb508f81d83c6f474db084462363e0b3b4b
JakobPaulsson <JakobPaulsson@users.noreply.github.com>
2023-11-20 22:40:05 +0000

    Prettified Code!

--- server/routes/connect.js
+++ server/routes/connect.js
@@ -2,13 +2,11 @@ const sqlite3 = require("sqlite3");
 const sqlite = require("sqlite");
 
 module.exports = {
-    connect: async function () {
-        const db = await sqlite.open({
-            filename: "../db/recipes.db",
-            driver: sqlite3.Database,
-        });
-        return db;
-    }
-}
-
-
+  connect: async function () {
+    const db = await sqlite.open({
+      filename: "../db/recipes.db",
+      driver: sqlite3.Database,
+    });
+    return db;
+  },
+};

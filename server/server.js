468660c65b909f66da94f432cda950df9b0dc819
JakobPaulsson <JakobPaulsson@users.noreply.github.com>
2023-11-21 11:59:03 +0000

    Prettified Code!

--- server/server.js
+++ server/server.js
@@ -1,19 +1,19 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const score = require("./routes/score")
-const user = require("./routes/user")
-const ingredient = require("./routes/ingredient")
-const recipes = require("./routes/recipes")
-const suggestions = require("./routes/suggestions")
+const score = require("./routes/score");
+const user = require("./routes/user");
+const ingredient = require("./routes/ingredient");
+const recipes = require("./routes/recipes");
+const suggestions = require("./routes/suggestions");
 
 app.use(cors());
 app.listen(8080, () => {
   console.log("server listening on port 8080");
 });
 
-score.initializeScoreRoute(app)
-user.initializeUserRoute(app)
-ingredient.initializeIngredientRoute(app)
-recipes.initializeRecipesRoute(app)
-suggestions.initializeSuggestionsRoute(app)
+score.initializeScoreRoute(app);
+user.initializeUserRoute(app);
+ingredient.initializeIngredientRoute(app);
+recipes.initializeRecipesRoute(app);
+suggestions.initializeSuggestionsRoute(app);

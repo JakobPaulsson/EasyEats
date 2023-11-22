dceb15da15c82e5c7c34cc35657adf61e4869c68
Robert Martinis <robertmartinis@hotmail.com>
2023-11-22 10:15:18 +0100

    Add headers for paper component

--- app/src/components/CustomPreset/CustomPreset.tsx
+++ app/src/components/CustomPreset/CustomPreset.tsx
@@ -2,12 +2,17 @@ import React from "react";
 import "./CustomPreset.css";
 import PresetIcon from "../../components/PresetIcon/PresetIcon";
 
-import { Container, TextField, Slider, Typography } from "@mui/material";
+import { Container, TextField, Slider, Typography, Paper } from "@mui/material";
+import PaperHeader from "../../components/PaperHeader/PaperHeader";
 
-function SliderComponent(props: any) {
+interface SliderComponentProps {
+  label: string;
+}
+
+function SliderComponent({ label }: SliderComponentProps) {
   return (
     <Container>
-      <Typography>{props.label}</Typography>
+      <Typography>{label}</Typography>
       <Slider step={0.1} min={0} max={1} marks valueLabelDisplay="auto" />
     </Container>
   );
@@ -16,27 +21,38 @@ function SliderComponent(props: any) {
 function CustomPreset() {
   return (
     <div className="outerContainer">
-      <div className="customPresetContainer">
-        <PresetIcon />
-        <TextField
-          sx={{ width: "80%", borderRadius: "50px" }}
-          id="outlined-basic"
-          label="Preset Name..."
-          variant="filled"
-        />
-        <div className="sliderGridContainer">
-          <SliderComponent label={"Previously Used Ingredients"} />
-          <SliderComponent label={"Something New"} />
-          <SliderComponent label={"Short Cook Time"} />
-          <SliderComponent label={"Rating"} />
-          <SliderComponent label={"Few Ingredients"} />
-          <SliderComponent label={"Ingredients in Inventory"} />
+      <Paper
+        elevation={6}
+        sx={{
+          width: 650,
+          height: 800,
+          mt: "60px",
+          borderRadius: "10px",
+        }}
+      >
+        <PaperHeader title="Create Custom Preset" />
+        <div className="customPresetContainer">
+          <PresetIcon />
+          <TextField
+            sx={{ width: "80%", borderRadius: "50px" }}
+            id="outlined-basic"
+            label="Preset Name..."
+            variant="filled"
+          />
+          <div className="sliderGridContainer">
+            <SliderComponent label={"Previously Used Ingredients"} />
+            <SliderComponent label={"Something New"} />
+            <SliderComponent label={"Short Cook Time"} />
+            <SliderComponent label={"Rating"} />
+            <SliderComponent label={"Few Ingredients"} />
+            <SliderComponent label={"Ingredients in Inventory"} />
+          </div>
+          <div className="buttonContainer">
+            <button className="button">Add Preset</button>
+            <button className="button">Cancel</button>
+          </div>
         </div>
-        <div className="buttonContainer">
-          <button className="button">Add Preset</button>
-          <button className="button">Cancel</button>
-        </div>
-      </div>
+      </Paper>
     </div>
   );
 }

2208bfe63f04662f8bb6e17d52262d1d8160e924
Robert Martinis <robertmartinis@hotmail.com>
2023-11-25 17:11:04 +0100

    add dynamic header

--- app/src/services/IngredientService.tsx
+++ app/src/services/IngredientService.tsx
@@ -12,7 +12,7 @@ export interface IngredientData {
 export const getIngredients = async (userID: number) => {
   try {
     const response: IngredientResponse = await axios.get(
-      `http://localhost:8080/ingredient?userID=${userID}`
+      `http://localhost:8080/ingredient?userID=${userID}`,
     );
     if (response && response.data) {
       localStorage.setItem("ingredients", JSON.stringify(response));
@@ -27,11 +27,11 @@ export const addIngredient = async (
   userID: number,
   ingredient: string,
   ingredientAmount: number,
-  ingredientUnit: string
+  ingredientUnit: string,
 ) => {
   try {
     await axios.post(
-      `http://localhost:8080/ingredient?userID=${userID}&ingredient=${ingredient}&ingredientAmount=${ingredientAmount}&ingredientUnit=${ingredientUnit}`
+      `http://localhost:8080/ingredient?userID=${userID}&ingredient=${ingredient}&ingredientAmount=${ingredientAmount}&ingredientUnit=${ingredientUnit}`,
     );
   } catch (error) {
     console.error("Failed to fetch search query", error);
@@ -43,7 +43,7 @@ export const addIngredient = async (
 export const removeIngredient = async (userID: number, ingredient: string) => {
   try {
     const response = await axios.delete(
-      `http://localhost:8080/ingredient?userID=${userID}&ingredient=${ingredient}`
+      `http://localhost:8080/ingredient?userID=${userID}&ingredient=${ingredient}`,
     );
     if (response && response.data) {
       return response;

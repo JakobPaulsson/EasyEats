81da662fc3231acad6168bfc4d0038926059a2c6
Robert Martinis <robertmartinis@hotmail.com>
2024-01-10 19:32:56 +0100

    fix: use userID in fetch

--- app/src/services/RecipeService.ts
+++ app/src/services/RecipeService.ts
@@ -5,12 +5,12 @@ axios.defaults.withCredentials = true;
 export const fetchSearchResults = async (
   ingredients: string,
   page: string | undefined,
-  userID: number,
+  userID: number
 ): Promise<RecipeResponse | undefined> => {
   try {
     const ingredientsQuery = `title=${ingredients}`;
     const response = await axios.get(
-      `${process.env.REACT_APP_HOST}/recipes/search?page=${page}&${ingredientsQuery}`,
+      `${process.env.REACT_APP_HOST}/recipes/search?page=${page}&${ingredientsQuery}&userID=${userID}`
     );
     if (response && response.data) {
       return response;
@@ -23,7 +23,7 @@ export const fetchSearchResults = async (
 export const fetchScored = async (page: string | undefined, userID: number) => {
   try {
     const response = await axios.get(
-      `${process.env.REACT_APP_HOST}/recipes?userID=${userID}&page=${page}`,
+      `${process.env.REACT_APP_HOST}/recipes?userID=${userID}&page=${page}`
     );
     if (response && response.data) {
       return response;
